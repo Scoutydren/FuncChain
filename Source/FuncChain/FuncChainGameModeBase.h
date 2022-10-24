6e0baa9b61e2c7674756d7222ce7162c61e99f14
Scoutydren <gubeini@seas.upenn.edu>
2022-10-24 01:29:37 -0400

    starter content and project initialization

---
+++ Source/FuncChain/FuncChainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FuncChainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNCCHAIN_API AFuncChainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
